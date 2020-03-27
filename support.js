1c722df0878f520c8230cb60b79624c4323c8936
jasoncollege24 <jasoncollege24@gmail.com>
2020-03-27 03:46:28 -0400

    Update support.js

--- support.js
+++ support.js
@@ -1,5 +1,5 @@
 //Support breakpoints
-	pingwarn="hang on, while I check with carl...";
+	pingwarn="hang on, while I check with Carl...";
 	cancelbreak="your ticket is closed. "+SupportRef+" will be with you soon.";
 	fixedbreak="I'll close your ticket, and mark it as resolved. If you need more help, come see me again!";
 	breakpoint="I can't help you with this just yet. "+SupportRef+", someone needs your assistance! (Ticket closed)"; 
