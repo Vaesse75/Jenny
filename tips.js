a871934a03fe88819da10faef6e799ba5627a018
jasoncollege24 <jasoncollege24@gmail.com>
2020-03-28 22:09:19 -0400

    bugfix6
    seriously?

--- tips.js
+++ tips.js
@@ -3,7 +3,7 @@
 if (input.match(/^\?tip/)) {
 	//tips
 	var say=[
-		"TIP: Did you know? It's normal to see a warning that you can't reach our Plex securely on first use (per device). Just select "Allow always" when prompted.",
+		"TIP: Did you know? It's normal to see a warning that you can't reach our Plex securely on first use (per device). Just select \"Allow always\" when prompted.",
 		"TIP: Did you know? Plex libraries always default to show \"Recommended\" items on first use. To show the entire library, change that to \"Library\", and you should be good to go."
 	];
 	msg.channel.send({ underlay(say[Math.floor(Math.random()*say.length)],tips) });
