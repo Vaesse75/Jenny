32638ba12bfb40af389367b6c07dec53451f135a
jasoncollege24 <jasoncollege24@gmail.com>
2020-03-29 00:22:23 -0400

    Update tips.js

--- tips.js
+++ tips.js
@@ -1,6 +1,6 @@
 // hidden links example "There's a pretty good description of the Habitica [classes](https://discordapp.com/channels/664197181846061077/664199483025915904/664219513172131843) in our adventure guide."
 
-module.exports=function(input) {
+module.exports=function(input,underlay) {
 		//tips
 		var say=[
 			"TIP: Did you know? It's normal to see a warning that you can't reach our Plex securely on first use (per device). Just select 'Allow always' when prompted.",
