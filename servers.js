ddf9e0753128ea56b1776a8fc6a2ec4f4fee6f48
jasoncollege24 <jasoncollege24@gmail.com>
2020-04-14 23:30:07 -0400

    Update servers.js

--- servers.js
+++ servers.js
@@ -1,47 +1,22 @@
-function Check(args,chan,pass) {
+// usage: variable=require(filename)(<plexmediaserver>|<calibre-server>|<proftpd>);
+module.exports=function(args) {
     var shellCommand = require("linux-shell-command").shellCommand;
-    for (var s=0;s<Server.length;s++) {
-        if (args == Server[s]) {
-            var sc = shellCommand("systemctl status "+ServerProc[s]+"|grep Active|while read a b c;do echo $b;done");
-            sc.execute()
-            .then(success => {
-                for (var r in Server) {
-                    if(args==Server[r]) s=r;
-                }
-                if (success === true && sc.stdout != "") {
-                    if (sc.stdout.slice(0,6) == "active") {
-                        return true;
-                    }
-                    else if (sc.stdout.slice(0,6) != "active") {
-                        return false;
-                    }
-                }
-            })
-            .catch(e => {
-                console.error(e);
-            });
+    var sc = shellCommand("systemctl status "+args+"|grep Active|while read a b c;do echo $b;done");
+    sc.execute()
+    .then(success => {
+        for (var r in Server) {
+            if (args==Server[r]) s=r;
         }
-        else if (args==ServerProc[s]) {
-            Check(Server[s],chan);
+        if (success === true && sc.stdout != "") {
+            if (sc.stdout.slice(0,6) == "active") {
+                return true;
+            }
+            else if (sc.stdout.slice(0,6) != "active") {
+                return false;
+            }
         }
-        else if (args==""||args=="all") {
-            Check(Server[s],chan,"all");
-        }
-    }
-    if(chan) Report(args,chan);
+    })
+    .catch(e => {
+        console.error(e);
+    });
 }
-    // Arrays of services and other related sundries.
-    Server=new Array("plex","calibre","ftp");
-    ServerProc=new Array("plexmediaserver","calibre-server","proftpd");
-
-    // Fill Online status array with indeterminant state.
-    for (args in Server) {
-        Online[Server[args]]="unknown";
-    }
-    // First check
-    Check('');
-
-    // Repeat checks
-     // setInterval(function() {Check('')},5000);
-        // ping reply
-module.exports=Check;
