06513e2467c860634f380dd958a9096ea121dc10
jasoncollege24 <jasoncollege24@gmail.com>
2020-05-21 05:42:25 -0400

    typo fix

--- tips.js
+++ tips.js
@@ -9,7 +9,7 @@ module.exports=function(msg,underlay) {
 			"TIP: Did you know? It's ok to see a warning that you can't reach our Plex securely on first use (may be device dependent). Just select 'Allow always' when prompted.",
 			"TIP: Did you know? Plex libraries always default to show 'Recommended' items on first use. To show the entire library, change that to 'Library', and you should be good to go.",
 			"TIP: Did you know? Each device uses Plex a bit differently, so what works on one, may not always work on another.",
-			"TIP: Did you know? Plex, Calibre, and other relevent services automatically get restarted around 3am (Eastern), for continued stability, and updates. In some cases, the entire server restarts.
+			"TIP: Did you know? Plex, Calibre, and other relevent services automatically get restarted around 3am (Eastern), for continued stability, and updates. In some cases, the entire server restarts.",
 			"TIP: Still see the episode, movie, or other item you just finished playing in 'On Deck', or 'Continue watching'? This is a known Plex bug! Click on another menu item, then back to the server's home, or refresh the page, and it will update."
 		];
 		msg.channel.send(underlay(say[Math.floor(Math.random()*say.length)],"tips"));
