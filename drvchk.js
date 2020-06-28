4f8fe3c3faf0aa3c9d6f618504c3c1399f5f04dd
jasoncollege24 <jasoncollege24@gmail.com>
2020-06-27 20:56:20 -0400

    Update drvchk.js

--- drvchk.js
+++ drvchk.js
@@ -3,21 +3,25 @@ module.exports=async function(chan,staff) {	// Drive checking
 	try {
 		let fstb=shell.shellCommand("cat '!?!'", ["/etc/fstab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
 		fstb.execute().
-		then(success=> if (success === true && fstb.stdout != "") {
-			let mtb=shell.shellCommand("cat '!?!'", ["/etc/mtab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
-			mtb.execute().
-			then(success => if (success === true && mtb.stdout != "") {
-				fstb=fstb.stdout.split(/\s+/);
-				let msng=fstb.map(drv => {
-					if (mtb.includes(drv)) return drv.slice(1);
-				});
-				console.log(fstb+"\n"+mtb+"\n"+msng);
-				if (msng.length>0) {
-					msgs=[
-						msng[1]+" has been reported missing"+(msng.length>1?", it was last seen in the company of "+(msng.length>2?msng.slice(1,-1).join(", ")+", and ":"")+msng.slice(-1):"")+"."
-					];
-					let say=Math.floor(Math.random()*msgs.length)||"burps.";
-					chan.send(say?staff+", "+say:"burps.");
+		then(success=> {
+			if (success === true && fstb.stdout != "") {
+				let mtb=shell.shellCommand("cat '!?!'", ["/etc/mtab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
+				mtb.execute().
+				then(success => {
+					if (success === true && mtb.stdout != "") {
+						fstb=fstb.stdout.split(/\s+/);
+						let msng=fstb.map(drv => {
+							if (mtb.includes(drv)) return drv.slice(1);
+						});
+						console.log(fstb+"\n"+mtb+"\n"+msng);
+						if (msng.length>0) {
+							msgs=[
+								msng[1]+" has been reported missing"+(msng.length>1?", it was last seen in the company of "+(msng.length>2?msng.slice(1,-1).join(", ")+", and ":"")+msng.slice(-1):"")+"."
+							];
+							let say=Math.floor(Math.random()*msgs.length)||"burps.";
+							chan.send(say?staff+", "+say:"burps.");
+						}
+					}
 				}
 			}
 		}
