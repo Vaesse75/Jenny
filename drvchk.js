30d7a409ca66f36ba1ec4e12d325ed6654ac1b60
jasoncollege24 <jasoncollege24@gmail.com>
2020-06-27 20:31:56 -0400

    Update drvchk.js

--- drvchk.js
+++ drvchk.js
@@ -1,15 +1,15 @@
 const shell = require('linux-shell-command');
 module.exports=async function(chan,staff) {	// Drive checking
 	try {
-		let fstb,mtb;
-		await fstb = shell.shellCommand("cat '!?!'", ["/etc/fstab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
-		await fstb=fstb.execute();
-		await mtb = shell.shellCommand("cat '!?!'", ["/etc/mtab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
-		await mtb=mtb.execute();
+		let await fstb = shell.shellCommand("cat '!?!'", ["/etc/fstab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
+		fstb=await fstb.execute();
+		let mtb = await shell.shellCommand("cat '!?!'", ["/etc/mtab|egrep -o '/media/plex/Plex-([^/])+$'|egrep -o '\-\S+'"]);
+		mtb=await mtb.execute();
 		fstb=fstb.stdout.split(/\s+/);
 		let msng=fstb.map(drv => {
 			if (mtb.includes(drv)) return drv.slice(1);
 		});
+		console.log(mtb+"\n"+fstb+"\n"+msng);
 		if (msng.length>0) {
 			msgs=[
 				msng[1]+" has been reported missing"+(msng.length>1?", it was last seen in the company of "+(msng.length>2?msng.slice(1,-1).join(", ")+", and ":"")+msng.slice(-1):"")+"."
