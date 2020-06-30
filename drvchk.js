c20439f57ed6ba5ec890608ff1ae1d5ba2161ac9
jasoncollege24 <jasoncollege24@gmail.com>
2020-06-30 00:08:26 -0400

    logic fix

--- drvchk.js
+++ drvchk.js
@@ -1,7 +1,6 @@
 const shell = require('linux-shell-command').shellCommand;
 module.exports=async function(chan,staff) {	// Drive checking
 	try {
-		//console.log(shell);
 		let fstb=shell("cat /etc/fstab");
 		fstb.execute()
 		.then(success=> {
@@ -16,14 +15,15 @@ module.exports=async function(chan,staff) {	// Drive checking
 						let m={};
 						mtb.stdout.match(/\/media\/plex\/Plex-([^\/]+?)\s/g).map(a=>{return a.slice(17,-1);}).forEach(a=>{m[a]=true;});
 						m=Object.keys(m);
-						let msng=f.map(drv=>{
-							if (m.includes(drv)) return drv;
+						let msng=[];
+						f.forEach(drv=>{
+							if (!m.includes(drv)) msng.push(drv);
 						});
 						if (msng.length>0) {
 							msgs=[
 								msng[1]+" has been reported missing"+(msng.length>1?", it was last seen in the company of "+(msng.length>2?msng.slice(1,-1).join(", ")+", and ":"")+msng.slice(-1):"")+"."
 							];
-							let say=Math.floor(Math.random()*msgs.length);
+							let say=msgs[Math.floor(Math.random()*msgs.length)];
 							console.log("Missing drives: "+msng+"\n"+say);
 							chan.send(say?staff+", "+say:"burps.");
 						}
