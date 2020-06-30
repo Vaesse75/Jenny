af751548697d5d396ab50aeaa40d18e470b66ec9
jasoncollege24 <jasoncollege24@gmail.com>
2020-06-30 00:12:27 -0400

    Update drvchk.js

--- drvchk.js
+++ drvchk.js
@@ -15,16 +15,16 @@ module.exports=async function(chan,staff) {	// Drive checking
 						let m={};
 						mtb.stdout.match(/\/media\/plex\/Plex-([^\/]+?)\s/g).map(a=>{return a.slice(17,-1);}).forEach(a=>{m[a]=true;});
 						m=Object.keys(m);
-						let msng=[];
-						f.forEach(drv=>{
-							if (!m.includes(drv)) msng.push(drv);
+						let msng=f.map(drv=>{
+							if (!m.includes(drv)) return drv;
 						});
+						//msng.push("Movies");
+						console.log("Missing drives: "+msng);
 						if (msng.length>0) {
 							msgs=[
 								msng[1]+" has been reported missing"+(msng.length>1?", it was last seen in the company of "+(msng.length>2?msng.slice(1,-1).join(", ")+", and ":"")+msng.slice(-1):"")+"."
 							];
 							let say=msgs[Math.floor(Math.random()*msgs.length)];
-							console.log("Missing drives: "+msng+"\n"+say);
 							chan.send(say?staff+", "+say:"burps.");
 						}
 					}
