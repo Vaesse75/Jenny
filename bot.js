3f6e9b530bf21ff15070ac29dad9aa3b30724d5a
jasoncollege24 <jasoncollege24@gmail.com>
2020-09-03 03:37:27 -0400

    ignore self, and listen for Carl

--- bot.js
+++ bot.js
@@ -106,184 +106,185 @@ Jenny.on('ready', () => {
 
 // Reply to messages
 Jenny.on('message', msg => {
-	require('./noproblemo.js')(msg);
-    var input=msg.content.toLowerCase();
-    var tag="<@"+msg.author.id+">";
-    //response modules
-    require('./social.js')(msg); // Social responses (Plain text)
-    require('./tips.js')(msg,underlay); //tips module (Programmatic)
- 
- //// Programatic triggers
-    //undocumented test trigger
-    if (input.match(/^\?test 0001/)) {
-        var plex=checkit("plexmediaserver");
-        var calibre=checkit("calibre-server");
-        var ftp=checkit("proftpd");
-        if (plex) {
-            var t="open!";
-        }
-        else if (!plex) {
-            var t="closed!";
-        }
-        if (calibre) {
-            var c="open!";
-        }
-        else if (!calibre) {
-            var c="closed!";
-        }
-        if (ftp) {
-            var f="up!";
-        }
-        else if (!ftp) {
-            var f="down!";
-        }
-        var say1="Theater is "+t;
-        var say2="Library is "+c;
-        var say3="FTP is "+f;
-        msg.channel.send(say1+" "+say2+" "+say3);
-    }
- 
-    // ping reply
-	if (input.match(/^\?ping/)) {
-        var say=new Array("Yup! You're here!");
-        msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-    }
-   // Bot banter
-	if (input=="my apologies, i was a bit distracted." && Math.floor(Math.random() * 10)==0) {
-		var say=[
-			"What's her name?",
-			"Umm... Do I really want to know?",
-			"At least somebody's having fun."
-		];
-		msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-	}
-	// support text
-	if (input.match(/^\?support/)) {
-        ticket[msg.author.id]=input.substr(9).split(" ");
-        var level=support;
-        /*if (ticket[msg.author.id][0] == "plex") {
-            var srvc="plexmediaserver";
-        }
-        if (ticket[msg.author.id][0] == "calibre") {
-            var srvc="calibre-server";
-        }
-        if (ticket[msg.author.id][0] == "ftp") {
-            var srvc="proftpd";
-        }*/
-        if (ticket[msg.author.id].length>0 && ticket[msg.author.id][0] != "") {
-            var keys="";
-            for (var key in level) {
-                if (keys != "") keys+=",";
-                keys+=key;
-            }
-            if (ticket[msg.author.id].length > 0 && keys.indexOf(ticket[msg.author.id][0]) >= 0) {
-                //waitForPing=true;
-                //Online=checkit(srvc);
-                waitForPing=ticket[msg.author.id][0];
-                suppconn.send(tag+", "+pingwarn);
-                suppconn.send("!ping "+ticket[msg.author.id][0]+" for "+tag);
+	if (client.user.id !== msg.author.id) {
+		if (msg.author.id === "675406803567378512" && nCarl) {
+			clearTimout(nCarl);
+		}
+		require('./noproblemo.js')(msg);
+		var input=msg.content.toLowerCase();
+		var tag="<@"+msg.author.id+">";
+		//response modules
+		require('./social.js')(msg); // Social responses (Plain text)
+		require('./tips.js')(msg,underlay); //tips module (Programmatic)
+	 
+	 //// Programatic triggers
+		//undocumented test trigger
+		if (input.match(/^\?test 0001/)) {
+			var plex=checkit("plexmediaserver");
+			var calibre=checkit("calibre-server");
+			var ftp=checkit("proftpd");
+			if (plex) {
+				var t="open!";
+			}
+			else if (!plex) {
+				var t="closed!";
+			}
+			if (calibre) {
+				var c="open!";
+			}
+			else if (!calibre) {
+				var c="closed!";
+			}
+			if (ftp) {
+				var f="up!";
+			}
+			else if (!ftp) {
+				var f="down!";
+			}
+			var say1="Theater is "+t;
+			var say2="Library is "+c;
+			var say3="FTP is "+f;
+			msg.channel.send(say1+" "+say2+" "+say3);
+		}
+	 
+		// ping reply
+		if (input.match(/^\?ping/)) {
+			var say=new Array("Yup! You're here!");
+			msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+		}
+	   // Bot banter
+		if (input=="my apologies, i was a bit distracted." && Math.floor(Math.random() * 10)==0) {
+			var say=[
+				"What's her name?",
+				"Umm... Do I really want to know?",
+				"At least somebody's having fun."
+			];
+			msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+		}
+		// support text
+		if (input.match(/^\?support/)) {
+			ticket[msg.author.id]=input.substr(9).split(" ");
+			var level=support;
+			/*if (ticket[msg.author.id][0] == "plex") {
+				var srvc="plexmediaserver";
+			}
+			if (ticket[msg.author.id][0] == "calibre") {
+				var srvc="calibre-server";
+			}
+			if (ticket[msg.author.id][0] == "ftp") {
+				var srvc="proftpd";
+			}*/
+			if (ticket[msg.author.id].length>0 && ticket[msg.author.id][0] != "") {
+				var keys="";
+				for (var key in level) {
+					if (keys != "") keys+=",";
+					keys+=key;
+				}
+				if (ticket[msg.author.id].length > 0 && keys.indexOf(ticket[msg.author.id][0]) >= 0) {
+					//waitForPing=true;
+					//Online=checkit(srvc);
+					waitForPing=ticket[msg.author.id][0];
+					suppconn.send(tag+", "+pingwarn);
+					suppconn.send("!ping "+ticket[msg.author.id][0]+" for "+tag);
+					
+				}
+				else {
+					ticket[msg.author.id]=[];
+					//suppconn.send(support[0]);
+				}
+			}
+			else {
+				ticket[msg.author.id]=[];
+				//suppconn.send(support[0]);
+			}
+		}
+
+		// If author has an open ticket
+		if (ticket[msg.author.id] && !input.match(' ')) {
+			var arr=ticket[msg.author.id];
+			var said=input.split(" ")[0];
+			var level=walkSupport(ticket[msg.author.id]);
+			var keys="";
+			if (ticket[msg.author.id][0] == "plex") {
+				var srvc="plexmediaserver";
+			}
+			if (ticket[msg.author.id][0] == "calibre") {
+				var srvc="calibre-server";
+			}
+			if (ticket[msg.author.id][0] == "ftp") {
+				var srvc="proftpd";
+			}
+			for (var key in level) {
+				if (keys != "") keys+=",";
+				keys+=key;
+			}
+			if (keys.indexOf(said) >= 0) {
+				arr.push(said);
+				level=walkSupport(arr);
+				var keys="";
+				for (var key in level) {
+					if (keys != "") keys+=",";
+					keys+=key;
+				}
+			}
+			if (said=="back") {
+				ticket[msg.author.id].pop();
+				level=walkSupport(ticket[msg.author.id]);
+				suppconn.send(tag+", "+level[0]);
+			}
+			else if (said=="fixed") {
+			 suppconn.send(tag+", "+fixedbreak);
+			 ticket[msg.author.id]=null;
+			}
+			else if (said=="cancel") {
+				suppconn.send(tag+", "+cancelbreak);
+				ticket[msg.author.id]=null;
+			}
+			else if (ticket[msg.author.id].length==1 && said != "?support") {
+				//waitForPing=checkit(srvc);
 				
-            }
-            else {
-                ticket[msg.author.id]=[];
-                //suppconn.send(support[0]);
-            }
-        }
-        else {
-            ticket[msg.author.id]=[];
-            //suppconn.send(support[0]);
-        }
-	}
-
-    // If author has an open ticket
-	if (ticket[msg.author.id] && !input.match(' ')) {
-        var arr=ticket[msg.author.id];
-        var said=input.split(" ")[0];
-        var level=walkSupport(ticket[msg.author.id]);
-        var keys="";
-        if (ticket[msg.author.id][0] == "plex") {
-            var srvc="plexmediaserver";
-        }
-        if (ticket[msg.author.id][0] == "calibre") {
-            var srvc="calibre-server";
-        }
-        if (ticket[msg.author.id][0] == "ftp") {
-            var srvc="proftpd";
-        }
-        for (var key in level) {
-            if (keys != "") keys+=",";
-            keys+=key;
-        }
-        if (keys.indexOf(said) >= 0) {
-            arr.push(said);
-            level=walkSupport(arr);
-            var keys="";
-            for (var key in level) {
-                if (keys != "") keys+=",";
-                keys+=key;
-            }
-        }
-        if (said=="back") {
-            ticket[msg.author.id].pop();
-            level=walkSupport(ticket[msg.author.id]);
-            suppconn.send(tag+", "+level[0]);
-        }
-        else if (said=="fixed") {
-         suppconn.send(tag+", "+fixedbreak);
-         ticket[msg.author.id]=null;
-        }
-		else if (said=="cancel") {
-			suppconn.send(tag+", "+cancelbreak);
-			ticket[msg.author.id]=null;
+				waitForPing=ticket[msg.author.id][0];
+				suppconn.send(tag+", "+pingwarn);
+				suppconn.send("!ping "+ticket[msg.author.id][0]+" for "+tag);
+				
 		}
-		else if (ticket[msg.author.id].length==1 && said != "?support") {
-            //waitForPing=checkit(srvc);
-            
-            waitForPing=ticket[msg.author.id][0];
-            suppconn.send(tag+", "+pingwarn);
-            suppconn.send("!ping "+ticket[msg.author.id][0]+" for "+tag);
-            
-    }
-        else if (typeof level == "string") {
-            suppconn.send(tag+", "+level);
-            ticket[msg.author.id]=null;
-        }
-        else  {
-            suppconn.send(tag+", "+level[0]);
-        }
- 
-    }
-	if (waitForPing) {
-		var nCarl;
-		function noCarl() {
-			//if (tag != "<@"+msg.author.bot+">") {
-				suppconn.send(tag+". "+breakpointCarl);
-				ticket[msg.author.id]=null;
-				clearTimeout(nCarl);
-			//}
-		}
-		if (input.match(/^[^,]*, (\w* ){2}is .*\.$/)) {
-			tag=input.split(",")[0];
-			if (input.substr(input.length-5,4)=="open" || input.substr(input.length-3,2)=="up") {
-				suppconn.send(tag+", "+support[waitForPing][0]);
-			}
-			else if (input.substr(input.length-7,6)=="closed" || input.substr(input.length-5,4)=="down") {
-				suppconn.send(tag+", "+breakpoint2);
+			else if (typeof level == "string") {
+				suppconn.send(tag+", "+level);
 				ticket[msg.author.id]=null;
 			}
-			waitForPing=false;
+			else  {
+				suppconn.send(tag+", "+level[0]);
+			}
+	 
 		}
-		else {
-			waitForPing=false;
-			nCarl=setTimeout(noCarl, 3000);
+		if (waitForPing) {
+			var nCarl;
+			function noCarl() {
+					suppconn.send(tag+". "+breakpointCarl);
+					ticket[msg.author.id]=null;
+			}
+			if (input.match(/^[^,]*, (\w* ){2}is .*\.$/)) {
+				tag=input.split(",")[0];
+				if (input.substr(input.length-5,4)=="open" || input.substr(input.length-3,2)=="up") {
+					suppconn.send(tag+", "+support[waitForPing][0]);
+				}
+				else if (input.substr(input.length-7,6)=="closed" || input.substr(input.length-5,4)=="down") {
+					suppconn.send(tag+", "+breakpoint2);
+					ticket[msg.author.id]=null;
+				}
+				waitForPing=false;
+			}
+			else {
+				waitForPing=false;
+				nCarl=setTimeout(noCarl, 3000);
+			}
+		}
+		// help text
+		if (input.match(/^\?help/)||input.match(/^help.*jenny.*/)) {
+			var say=new Array(Mbr(msg.member,0)+", here's a quick help list!\n\n?ping - Asks me to check if you're online.\n?support - Opens a trouble ticket (Automated support is in Beta, and requires Carl to be online.).\n?tip - tells me to give you a random support tip. (New!)\n?help - Tells me to display this message.\n\nNeed help from Carl? type !help to see what he can do!\n\nIf you need assistance or have feedback about my service, let a member of our Casting staff know in "+HelpRef+".");
+			msg.channel.send(say[Math.floor(Math.random()*say.length)]);
 		}
 	}
-    // help text
-	if (input.match(/^\?help/)||input.match(/^help.*jenny.*/)) {
-        var say=new Array(Mbr(msg.member,0)+", here's a quick help list!\n\n?ping - Asks me to check if you're online.\n?support - Opens a trouble ticket (Automated support is in Beta, and requires Carl to be online.).\n?tip - tells me to give you a random support tip. (New!)\n?help - Tells me to display this message.\n\nNeed help from Carl? type !help to see what he can do!\n\nIf you need assistance or have feedback about my service, let a member of our Casting staff know in "+HelpRef+".");
-        msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-	}
-
 });
 
 Jenny.login(auth.token);
